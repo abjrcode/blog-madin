3b3fa06fba60b94572f82742b6cb8990f6c933d5
Ibrahim Najjar <ABnajjar@hotmail.com>
2023-09-09 16:37:05 +0200

    Set the website typography and update description

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./blog/templates/*.html"],
   darkMode: "class",
   theme: {
+    fontFamily: {
+      sans: ["Lato", "Roboto", ...defaultTheme.fontFamily.sans],
+      mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono],
+    },
     debugScreens: {
       prefix: "dbg-screen: ",
       position: ["bottom", "right"],
